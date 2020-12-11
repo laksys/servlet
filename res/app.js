b676b442240af7c1f98de051b1129d418880e58a
lakshmanan <laksys@gmail.com>
2020-12-11 23:01:27 +0530

    dynamically added element

--- res/app.js
+++ res/app.js
@@ -1,1 +1,24 @@
-console.log('hello, world!')
+//create button element
+var btn = document.createElement("button")
+
+//add button caption
+btn.innerText = "Click Me!";
+
+//register button click event
+btn.addEventListener("click", function(event) {
+	//if button is clicked add style class 'red' to paragraph
+	p.classList.add("red")	
+}, false)
+
+//create paragraph element
+var p = document.createElement("p")
+
+//add it content
+p.innerText="Welcome to wonderful world of computers!"
+
+//append the button to the document
+document.body.appendChild(btn);
+
+//append the paragraph to the document
+document.body.appendChild(p);
+
